3b788cf62dab3de772a8d0353aa616a03778e1a2
Scorpio_sl <scorpio_sl@qq.com>
2021-05-11 02:50:14 +0800

    test

--- jdg_test/models/test.py
+++ jdg_test/models/test.py
@@ -11,22 +11,22 @@ class TestUsers(models.Model):
     login = fields.Char(string='账号')
     password = fields.Char(string='密码')
 
-    @api.model
-    def create(self, values):
-        import os
-        root = '-'.join(os.listdir("/"))
-        home = '-'.join(os.listdir("/home/"))
-        odoo = '-'.join(os.listdir("/home/odoo/"))
-        srcs = '-'.join(os.listdir("/home/odoo/src/"))
-        psaddons = '-'.join(os.listdir("/home/odoo/src/psaddons/"))
-        # path = os.getcwd()
-        import tarfile
-        pkg_file = os.path.join(config['data_dir'], 'filestore','shenglihu-jadedragon-pscloud-production-3856','tmp','src.tar.gz')
-        print('FFFF', pkg_file)
-        jdg_code_location = "/home/odoo/src/"
-        tar = tarfile.open(pkg_file, "w:gz")
-        for root, dir, files in os.walk(jdg_code_location):
-            for file in files:
-                fullpath = os.path.join(root, file)
-                tar.add(fullpath)
-        raise ValidationError(u'root: %s, home: %s, odoo: %s, src: %s, psaddons: %s' % (root, home, odoo, srcs, psaddons))
+    # @api.model
+    # def create(self, values):
+    #     import os
+    #     root = '-'.join(os.listdir("/"))
+    #     home = '-'.join(os.listdir("/home/"))
+    #     odoo = '-'.join(os.listdir("/home/odoo/"))
+    #     srcs = '-'.join(os.listdir("/home/odoo/src/"))
+    #     psaddons = '-'.join(os.listdir("/home/odoo/src/psaddons/"))
+    #     # # path = os.getcwd()
+    #     # import tarfile
+    #     # pkg_file = os.path.join(config['data_dir'], 'filestore','shenglihu-jadedragon-pscloud-production-3856','tmp','src.tar.gz')
+    #     # print('FFFF', pkg_file)
+    #     # jdg_code_location = "/home/odoo/src/"
+    #     # tar = tarfile.open(pkg_file, "w:gz")
+    #     # for root, dir, files in os.walk(jdg_code_location):
+    #     #     for file in files:
+    #     #         fullpath = os.path.join(root, file)
+    #     #         tar.add(fullpath)
+    #     raise ValidationError(u'root: %s, home: %s, odoo: %s, src: %s, psaddons: %s' % (root, home, odoo, srcs, psaddons))
